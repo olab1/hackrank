40506edccd22779aa1a28e203b486b9011d86368
Roger Xia <rxia@iselect.com.au>
2016-04-03 23:18:13 +1000

    d

--- hackerrank/src/rxia/number/ana/TestCase.java
+++ hackerrank/src/rxia/number/ana/TestCase.java
@@ -15,8 +15,9 @@ public class TestCase {
 	
 	@Test
 	public void startTEST() throws IOException{//fdsdad
- 		File file = new File("src\\rxia\\play\\number/sample.txt");
-		//THIS IS RXIA TEST
+ 
+		File file = new File("src\\rxia\\play\\number/sample.txt");
+ 		//THIS IS RXIA TEST
 		Solution solution = spy(  new Solution());
 		List<String> lines = FileUtils.readLines(file);
 		System.out.println(lines);
