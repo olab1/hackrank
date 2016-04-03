2473ed5a97d58652dfd1772d0dd617d2dfa8337c
Roger Xia <rxia@iselect.com.au>
2016-04-03 23:21:40 +1000

    dada

--- hackerrank/src/rxia/number/ana/TestCase.java
+++ hackerrank/src/rxia/number/ana/TestCase.java
@@ -1,3 +1,4 @@
+<<<<<<< 73082bd1a81d02e85a43269d22bcc0fe49ad605a
 package rxia.number.ana;
 
 import java.io.File;
@@ -41,3 +42,47 @@ public class TestCase {
 	}
 }
  
+=======
+package rxia.number.ana;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+
+
+public class TestCase {
+	
+	@Test
+ 	public void startTEST() throws IOException{
+		File file = new File("src\\rxia\\play\\number/sample.txt");
+		//THIS IS RXIA TEST
+		Solution solution = spy(  new Solution());
+		List<String> lines = FileUtils.readLines(file);
+		System.out.println(lines);
+		//int[] x = new int[5000*100000*10];
+		// doReturn(new Integer(Integer.parseInt(lines.get(0)))).when(solution).getLengthN();
+		// doReturn(convert(lines.get(1).trim().split(" "))).when(solution).getArrayN(Long.parseLong(lines.get(0)));
+		// doReturn(new Integer(Integer.parseInt(lines.get(2)))).when(solution).getLengthQ();
+		// doReturn(convert(lines.get(3).trim().split(" "))).when(solution).getArrayQ(Long.parseLong(lines.get(2)));
+		
+		// System.out.println(solution.getLengthN()+" "+solution.getLengthQ());
+		//solution.execute();
+ 		System.out.println("aa");
+	}
+	
+	private Long[] convert(String[] array){
+		Long[] temp = new Long[array.length];
+		int x = 0;
+		for(String a: array){
+			temp[x++] = Long.parseLong(a);
+		}
+		return temp;
+	}
+}
+>>>>>>> dada
