9e632b4a21e032dce6eb7ddf50cbd0c91c36cc2c
Roger Xia <rxia@iselect.com.au>
2016-04-03 23:04:11 +1000

    add

--- hackerrank/src/rxia/number/ana/TestCase.java
+++ hackerrank/src/rxia/number/ana/TestCase.java
@@ -2,6 +2,7 @@ package rxia.number.ana;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
@@ -13,11 +14,13 @@ import org.junit.Test;
 public class TestCase {
 	
 	@Test
-	public void startTEST() throws IOException{
-		File file = new File("src\\rxia\\play\\number/sample.txt");
+	public void startTEST() throws IOException, URISyntaxException{
+		File file = new File("src\\"+this.getClass().getPackage().getName().replace(".", "\\")+"\\sample.txt");
+		//System.out.println("src\\"+this.getClass().getPackage().getName().replace(".", "\\")+"\\sample.txt");
 		
-		Solution solution = spy(  new Solution());
+		Solution solution = spy(new Solution());
 		List<String> lines = FileUtils.readLines(file);
+		System.out.println(lines);
 		//int[] x = new int[5000*100000*10];
 		// doReturn(new Integer(Integer.parseInt(lines.get(0)))).when(solution).getLengthN();
 		// doReturn(convert(lines.get(1).trim().split(" "))).when(solution).getArrayN(Long.parseLong(lines.get(0)));
@@ -25,7 +28,7 @@ public class TestCase {
 		// doReturn(convert(lines.get(3).trim().split(" "))).when(solution).getArrayQ(Long.parseLong(lines.get(2)));
 		
 		// System.out.println(solution.getLengthN()+" "+solution.getLengthQ());
-		solution.execute();
+		//solution.execute();
  		System.out.println("aa");
 	}
 	
